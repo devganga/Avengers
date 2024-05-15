c9219f74f239ee6a86f5fc4264307a4f16c58227
Bharath <devgangabharat@gmail..com>
2024-05-15 22:53:28 +0530

    create quote item and entity framework

---
+++ src/Application/QuoteItems/Commands/CreateQuoteItem/CreateQuoteItem.cs
@@ -0,0 +1,57 @@
+﻿using Avengers.Application.Common.Interfaces;
+using Avengers.Domain.Entities;
+using Avengers.Domain.Events;
+
+namespace Avengers.Application.QuoteItems.Commands.CreateQuoteItem;
+
+public record CreateQuoteItemCommand : IRequest<int>
+{
+    public string? Title { get; init; }
+    public string? Note { get; init; }
+    public int? Rating { get; init; }
+    public string? Comments { get; init; }
+    public string? Source { get; init; }
+    public string? Author { get; init; }
+    public string? ReferenceUrl { get; init; }
+    public string? Tags { get; init; }
+}
+
+public class CreateQuoteItemCommandValidator : AbstractValidator<CreateQuoteItemCommand>
+{
+    public CreateQuoteItemCommandValidator()
+    {
+    }
+}
+
+public class CreateQuoteItemCommandHandler : IRequestHandler<CreateQuoteItemCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateQuoteItemCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> Handle(CreateQuoteItemCommand request, CancellationToken cancellationToken)
+    {
+        var entity = new QuoteItem
+        {
+            Title = request.Title,
+            Note = request.Note,
+            Tags = request.Tags,
+            Author = request.Author,
+            Comments = request.Comments,           
+            Rating = request.Rating,    
+            Source = request.Source,
+           ReferenceUrl = request.ReferenceUrl,
+        };
+
+        //entity.AddDomainEvent(new TodoItemCreatedEvent(entity));
+
+        _context.QuoteItems.Add(entity);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return entity.Id;
+    }
+}
---
+++ src/Domain/Entities/QuoteItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avengers.Domain.Entities;
+public class QuoteItem : BaseAuditableEntity
+{
+    public string? Title { get; set; }
+    public string? Note { get; set; }
+    public int? Rating { get; set; }
+    public string? Comments { get; set; }
+    public string? Source { get; set; }
+    public string? Author { get; set; }
+    public string? ReferenceUrl { get; set; }
+    public string? Tags { get; set; }
+}
